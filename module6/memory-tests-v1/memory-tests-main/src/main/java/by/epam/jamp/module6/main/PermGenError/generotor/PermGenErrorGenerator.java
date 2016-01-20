4b4dbb2d2529646a8be3cd59681cafc558b224a1
Vadzim Dzeravianko <vadim.by@gmail.com>
2016-01-20 16:21:03 +0300

    Task: Configuration

--- module6/memory-tests-v1/memory-tests-main/src/main/java/by/epam/jamp/module6/main/PermGenError/generotor/PermGenErrorGenerator.java
+++ module6/memory-tests-v1/memory-tests-main/src/main/java/by/epam/jamp/module6/main/PermGenError/generotor/PermGenErrorGenerator.java
@@ -16,15 +16,15 @@ public class PermGenErrorGenerator implements MemoryTest {
 			CtClass c = cp.makeClass("by.epam.jamp.module6.main.PermGenError.generotor.tmp.A" + i);
 
 			try {
-				StringBuilder sb = new StringBuilder();
-				sb.append("public int xmove(long dx) {");
-				for (int j = 0; j < 6; j++) {
-					//sb.append("long[] xy1"+j+" =  new long[300000]; ");
-					sb.append("long x"+j+" =  dx; ");	
-				}
-				
-				sb.append("return 1;}");
-				CtMethod m = CtNewMethod.make(sb.toString(), c);
+//				StringBuilder sb = new StringBuilder();
+//				sb.append("public int xmove(long dx) {");
+//				for (int j = 0; j < 6; j++) {
+//					//sb.append("long[] xy1"+j+" =  new long[300000]; ");
+//					sb.append("long x"+j+" =  dx; ");	
+//				}
+//				
+//				sb.append("return 1;}");
+//				CtMethod m = CtNewMethod.make(sb.toString(), c);
 				
 				Class clazz = c.toClass();
 				
